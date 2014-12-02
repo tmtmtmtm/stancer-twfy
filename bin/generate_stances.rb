90e566a9f7776311df65ebce7b9b68ad8c93a135
Tony Bowden <tony@tmtm.com>
2014-12-02 12:55:24 +0200

    feat: take optional command-line bloc

--- bin/generate_stances.rb
+++ bin/generate_stances.rb
@@ -3,6 +3,9 @@
 require 'json'
 require 'stancer'
 
+# usually 'voter' or 'group'
+group_by = ARGV.first || 'group'
+
 stancer = Stancer.new(
   sources: { 
     issues:  'https://raw.githubusercontent.com/tmtmtmtm/stance-viewer-sinatra/8cb8879092a211ffa3f2626c7e97aaea8897f730/data/issues.json', #FIXME
@@ -10,5 +13,5 @@ stancer = Stancer.new(
   }
 )
 
-stances = stancer.all_stances(group_by: 'group', exclude: 'indicators')
+stances = stancer.all_stances(group_by: group_by, exclude: 'indicators')
 puts JSON.pretty_generate(stances)
